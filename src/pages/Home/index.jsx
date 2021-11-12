05c1bb422e41fcdd7aedaaa88ed6580b147a1c4b
paulo-cds <paulo.pdomingues@hotmail.com>
2021-11-11 22:38:31 -0300

    conteudo lateral inicio carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,28 @@
-import React from "react"
-import {Container} from "./styles"
+import React, {useState} from "react"
+import TextField, {Input} from "@material/react-text-field"
+import MaterialIcon from "@material/react-material-icon"
+import {Container, Search, Logo, Wrapper, Map} from "./styles"
+import logo from "../../assets/logo.svg"
 
-const Home = () => <Container>Hello World</Container>
+const Home = () => {
+  const [inputValue, setInputValue] = useState("")
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo do Restaurante" />
+          <TextField
+            label="Pesquisar Restaurante"
+            outlined
+            trailingIcon={<MaterialIcon role="button" icon="search" />}
+          >
+            <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+          </TextField>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
+  )
+}
 
 export default Home
